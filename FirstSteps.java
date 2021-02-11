a4a2be604723ed5be02dea39af01a79c76043446
Ioana Damoc <ioanadamoc@yahoo.com>
2021-02-11 19:03:46 +0000

    second

---
+++ FirstSteps.java
@@ -0,0 +1,13 @@
+//Task 1.1 Write a program that displays your name, address and e-mail address, each on separate lines
+public class FirstSteps
+{
+   public static void main(String [] args)
+   {
+      System.out.println("Ioana Damoc");
+//Task 1.2 Adapt this to have a blank line between name and address
+      System.out.println(); //print an empty line for task 1.2     
+      System.out.println("2 Learning Street, Dudley");
+      System.out.println("ioanadamoc@gmail.com");
+//Task 1.3 Write a program that shows your initials in asterisks.      
+   }
+}
