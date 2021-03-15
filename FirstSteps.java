7edcbb00749e3dec361cee9b5ee904f356873192
Ioana Damoc <ioanadamoc@yahoo.com>
2021-03-15 20:09:26 +0000

    not a change

--- FirstSteps.java
+++ FirstSteps.java
@@ -13,7 +13,6 @@ public class FirstSteps
      System.out.println("**");
      System.out.println("**");
      System.out.println();
-     //System.out.println();
      System.out.println("**   ****");
      System.out.println("**   **  **");
      System.out.println("**   **  **");
